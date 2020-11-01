1c5ccee722c1c7ab248af1bbd0bc959afde6847c
zahra-hm <zahra520@hotmail.com>
2020-11-01 19:12:55 +0000

    returns the corresponding key, based on entered value. using .find() function

---
+++ findKeyByValue.js
@@ -0,0 +1,26 @@
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    //console.log("✅✅✅ Assertion Passed: " + [actual] + " === " + [expected]);
+    console.log(`✅✅✅ Assertion Passed: ${actual} ===  ${expected}`);
+
+  } else if (actual !== expected) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !==  ${expected}`);
+
+  }
+
+};
+
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+const findKeyByValue = function(obj, value) {
+  return Object.keys(obj).find(key => obj[key] === value);
+}
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
