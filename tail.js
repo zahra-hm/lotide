21ac78220e0a31cc1db0ec35759ab1624cdf35c2
zahra-hm <zahra520@hotmail.com>
2020-11-03 05:59:40 +0000

    tail function refactores

--- tail.js
+++ tail.js
@@ -16,17 +16,19 @@ const assertEqual = function(actual, expected) {
   }
 };
 
+module.exports = tail;
+
 // This doesn't change the assertEqual function, but instead compares the each item in the array, and the length of the arrays, to see if they're equal
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+// const result = tail(["Hello", "Lighthouse", "Labs"]);
+// assertEqual(result.length, 2); // ensure we get back two elements
+// assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+// assertEqual(result[1], "Labs"); // ensure second element is "Labs"
 
 // Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// tail(words); // no need to capture the return value since we are not checking it
+// assertEqual(words.length, 3); // original array should still have 3 elements!
 
 /* This also works to compare the arrays by changing the assertEqual function
 
