34b610af9aca01ec3bfa484c9c09377a2bae6e37
zahra-hm <zahra520@hotmail.com>
2020-11-30 15:31:52 +0000

    deleted extra unncecessary code/comments

--- tail.js
+++ tail.js
@@ -8,7 +8,6 @@ tail(["Hello", "Lighthouse", "Labs"]);
 const assertEqual = function(actual, expected) {
 
   if (actual === expected) {
-    //console.log("✅✅✅ Assertion Passed: " + [actual] + " === " + [expected]);
     console.log(`✅✅✅ Assertion Passed: ${actual} ===  ${expected}`);
 
   } else if (actual !== expected) {
@@ -21,7 +20,6 @@ module.exports = tail;
 
 // Test Case: Check the original array 
 const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
 assertEqual(words.length, 3); // original array should still have 3 elements!
 
 assertEqual(tail(words).length, 2); // should PASS
