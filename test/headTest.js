41492cdc3f7b5ddb64256388bb73d211e80ded30
zahra-hm <zahra520@hotmail.com>
2020-11-30 22:01:36 +0000

    refactored with mocha and chai

--- test/headTest.js
+++ test/headTest.js
@@ -1,3 +1,9 @@
+// const head = require('../head');
+// const assertEqual = require('../assertEqual');
+
+// assertEqual(head([5,6,7]), 5);
+// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Helloo");
+
 const assert = require('chai').assert;
 const head   = require('../head');
 
